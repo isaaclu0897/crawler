e137c1d849658f4cfc914eef93bbe1a9462d09ac
wei <t104306033@ntut.org.tw>
2017-12-14 23:08:13 +0800

    update netbug_ptt, add set & deal with of push_number, package for code (Module) change the platform of system, from windows to linux

---
+++ ptt/nbtool.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 18 18:12:58 2017
+
+@author: wei
+"""
+import os
+import requests 
+from bs4 import BeautifulSoup
+
+class change_page():
+    ''' 換頁函數
+    
+    說明: 抽取 url 的 index 置換成下個頁面的 index
+    '''
+    def index(url, index):
+        ''' 索引換頁
+        
+        換到第2312頁
+        ex: url = 'https://www.ptt.cc/bbs/Beauty/index2102.html'
+            change_page.index(url, 2312)
+            
+            # https://www.ptt.cc/bbs/Beauty/index2312.html
+        '''
+        if not str(index).isdigit():
+            print(123)
+        url = url.split('/')
+        url[-1] = ('index{}.html'.format(index))
+        url = '/'.join(url)
+        print(url)
+        return url
+    def page(url, jump_page=1):
+        ''' 頁數換頁
+       
+        換1頁
+        ex: 
+            url = 'https://www.ptt.cc/bbs/Beauty/index2102.html'
+            change_page.page(url)
+            
+            # https://www.ptt.cc/bbs/Beauty/index2103.html
+        換3頁
+        ex: 
+            url = 'https://www.ptt.cc/bbs/Beauty/index2102.html'
+            change_page.page(url, 3)
+            
+            # https://www.ptt.cc/bbs/Beauty/index2105.html
+        向前換2頁
+        ex: 
+            url = 'https://www.ptt.cc/bbs/Beauty/index2102.html'
+            change_page.page(url, -2)
+            
+            # https://www.ptt.cc/bbs/Beauty/index2100.html
+        '''            
+        index = str(int(''.join([index_num for index_num in url if index_num.isdigit()])) + jump_page) 
+        url = url.split('/')
+        url[-1] = ('index{}.html'.format(index))
+        url = '/'.join(url)
+        print(url)
+        return url
+    
+def parse_url(url, specific=False, href=None):
+    ''' 解析網頁內容
+    
+    說明: 將 url 轉換成 html 以便抽取
+    -----
+    解析給定網頁
+    ex: url = 'https://www.ptt.cc/bbs/Beauty/index2102.html'
+        pares_url(url)
+        
+        擷取
+        #   <div class="r-ent">
+            <div class="nrec"><span class="hl f1">爆</span></div>
+            <div class="mark"></div>
+            <div class="title">
+            <a href="/bbs/Beauty/M.1490441716.A.0CA.html">[正妹] 假日合輯</a>
+            </div>
+            <div class="meta">
+            <div class="date"> 3/25</div>
+            <div class="author">Kyle5566</div>
+            </div>
+            </div>
+    解析給定網域及特定子題
+    ex: domain = 'https://www.ptt.cc'
+        specific_url = '/bbs/Beauty/M.1490441716.A.0CA.html'
+        parse_url(domain, specific=True, href=specific_url)
+        
+        擷取
+        #   <a href="http://i.imgur.com/snPJtnI.jpg" rel="nofollow" target="_blank">http://i.imgur.com/snPJtnI.jpg</a>
+            <a href="http://i.imgur.com/MGyzVsn.jpg" rel="nofollow" target="_blank">http://i.imgur.com/MGyzVsn.jpg</a>
+            <a href="http://i.imgur.com/HsgmPx2.jpg" rel="nofollow" target="_blank">http://i.imgur.com/HsgmPx2.jpg</a>
+            <a href="http://i.imgur.com/nBaQ1Rn.jpg" rel="nofollow" target="_blank">http://i.imgur.com/nBaQ1Rn.jpg</a>
+            <a href="http://i.imgur.com/L369uMP.jpg" rel="nofollow" target="_blank">http://i.imgur.com/L369uMP.jpg</a>
+            <a href="http://i.imgur.com/zlVGsFX.jpg" rel="nofollow" target="_blank">http://i.imgur.com/zlVGsFX.jpg</a>
+            <a href="http://i.imgur.com/Psw6R1v.jpg" rel="nofollow" target="_blank">http://i.imgur.com/Psw6R1v.jpg</a>
+            <a href="http://i.imgur.com/wMwkOcm.jpg" rel="nofollow" target="_blank">http://i.imgur.com/wMwkOcm.jpg</a>
+            <a href="http://i.imgur.com/FpvANo2.jpg" rel="nofollow" target="_blank">http://i.imgur.com/FpvANo2.jpg</a>
+    '''
+    if specific == True:
+        url += href
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'lxml')
+    del res
+    return soup
+
+def mk_exclusivedir(path, dirname):
+    ''' 創見專屬資料夾
+    
+    說明: 在路徑 path 中, 創見名為 dirname 的資料夾
+    -----
+    ex: 
+        path = '/home/wei/data/python/photo'
+        dirname = 'good'
+        os.path.isdir(path + '/' + dirname) # False
+        mk_exclusivedir(path, dirname)
+        os.path.isdir(path + '/' + dirname) # True
+    '''
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    os.chdir(path)
+    if not os.path.isdir(dirname):
+        os.mkdir('{}'.format(dirname))
+class ptt():
+    ''' ptt特定函數
+    '''
+    def deal_likenum(obj):
+        ''' ptt推文數處裡
+        
+        說明: ptt 推文數會有文字"爆"及"空", 需要處理掉才能過濾讚數
+        '''
+        try:
+            likenum = int(obj)
+        except:
+            if obj == '爆':
+                likenum = 99
+            elif obj == '':
+                likenum = -10
+            elif obj == 'X1':
+                likenum = -200
+            else:
+                likenum = 0
+        return likenum
+    
+    
+
--- ptt/netbug_ptt.py
+++ ptt/netbug_ptt.py
@@ -1,74 +1,63 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 18 21:43:49 2017
+Created on Sat Sep 18 00:14:26 2017
 
-@author: Wei
+@author: wei
 """
-
-import os
-import requests 
-from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 import re
 import time
+from nbtool import change_page, parse_url, mk_exclusivedir, ptt
 
-#二更，目標頁面爬取更新，新增連續下載，交互式輸入
+#三更，可按讚數下載, 及模組化
 
 start = time.time()
 
-# 目標頁面
+# 目標頁
 '''
 page_target = str(input('請輸入你想下載的ptt網頁\nex:https://www.ptt.cc/bbs/Beauty/index2101.html\n>>>'))
 page_number = int(input('請輸入想要下載的頁數\nex:2\n>>>'))
 path = str(input('請輸入你想要存放的位置\nex:C:\\Users\\VX\\Downloads\\123123\n>>>'))
 '''
-page_target = 'https://www.ptt.cc/bbs/Beauty/index2101.html'
+page_target = 'https://www.ptt.cc/bbs/Beauty/index2102.html'
 page_number = 5
-path = '/home/wei/data/python/photo'
+path = '/home/wei/data/python'
 
 count = 0
-add = 0
-# 跳轉到下一個目標頁面
+page_target = change_page.page(page_target, -1) # 換頁
+# 跳轉到下一個頁面
 while count<=page_number:
-    chnum = str(int(''.join([x for x in page_target if x.isdigit()])) + add) # 抽取 url index置換成下個頁面的 index
-    page_target = page_target.split('/')
-    page_target[-1] = ('index{}.html'.format(chnum))
-    page_target = '/'.join(page_target)
-    print(page_target)
-    add = 1
+    page_target = change_page.page(page_target)
     count += 1
-
-    res = requests.get(page_target)
-    soup = BeautifulSoup(res.text, 'lxml')
-    #進入到目標頁面中的每個主題頁面
-    for page in soup.select('.r-ent a'):
-        url = 'https://www.ptt.cc' + page['href']
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, 'lxml')
-        del res
-        
-        #判斷網址中有沒有圖片
-        if len(soup.findAll('a', {'href':re.compile('http:\/\/i\.imgur\.com\/.*')})) > 0:
-            try:
-            
-                #將路徑cd到欲下載位置，並新增專屬資料夾
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-                os.chdir(path)
-                if not os.path.isdir(page.text):
-                    os.mkdir('{}'.format(page.text))
-                for index, img_url in enumerate(soup.findAll('a', {'href':re.compile('http:\/\/i\.imgur\.com\/.*')})):
-                    try:
-                        urlretrieve(img_url['href'], path + '/{}/{}_{}.jpg'.format(page.text ,page.text, index))
-                    except:
-                        print('{} {}_{}.jpg 下載失敗!'.format(img_url['href'], page.text, index))
-                    finally:
-                        del index
-            except:
-                print('{} can not download {}'.format(page.text, url))
-                                    
-        print('{} finish'.format(page.text))
-        
-                                    
+    soup = parse_url(page_target)   # 頁面解析
+    # 進入到目標頁面中的每個主題頁面
+    for page in soup.select('.r-ent'):
+        likenumber = page.select('.nrec')[0].text
+        name = page.select('a')[0].text
+        likenum = ptt.deal_likenum(likenumber) # ptt推文數處裡
+        # 推文數大於50才下載
+        if likenum>50:
+            print(likenum, name,  '下載中...')
+            domain = 'https://www.ptt.cc'
+            soup = parse_url(domain, specific=True, href=page.select('.title a')[0]['href'])
+            # 判斷網址中有沒有圖片
+            if len(soup.findAll('a', {'href':re.compile('http:\/\/i\.imgur\.com\/.*')})) > 0:
+                try:
+                    # 將路徑cd到欲下載位置，並新增專屬資料夾
+                    mk_exclusivedir(path, likenumber+name) # 創建專屬資料夾
+                    for index, img_url in enumerate(soup.findAll('a', {'href':re.compile('http:\/\/i\.imgur\.com\/.*')})):
+                        try:
+#                            print(type(img_url['href']))
+#                            print(img_url['href'] + '...')
+                            print(name, index)
+                            #記得更改想要下載到的位置
+                            urlretrieve(img_url['href'], path + '/{}/{}_{}.jpg'.format(likenumber+name ,name, index))
+                        except:
+                            print('url: {} {}_{}.jpg 下載失敗!'.format(img_url['href'], name, index))
+                        finally:
+                            del index
+                except:
+                    print('{} can not download {}'.format(name, page))
+    print('{} finish'.format(name))
 print('程式共花費:{:f}秒'.format(time.time() - start))
-
