d914507cea79fd0487b12811d4f415c6fa6bdbca
wei <t104306033@ntut.org.tw>
2018-05-17 15:48:20 +0800

    /sciencedirect: start download

--- sciencedirect/pdf_download.py
+++ sciencedirect/pdf_download.py
@@ -19,16 +19,16 @@ def download(filename, src_url):
         soup = BeautifulSoup(src_res.text, "lxml")
         paper_url = soup.body.div.p.a['href']
         
-        print('start', filename)
+#        print('start', filename)
         paper_res = requests.get(paper_url, stream=True)
         filename = filename
         with open(filename, 'wb') as f:
             f.write(paper_res.content)
-        print('Completed', filename)
+#        print('Completed', filename)
     except Exception as e:
-        print(e.message, e.args)
-        return (filename, src_url)
-    return None
+        print(filename, src_url)
+#        return (filename, src_url, e)
+#    return None
 
 def index_date(attr):
     if '2017,' in attr:
@@ -43,6 +43,11 @@ def index_date(attr):
         date = 'unknow_'
     return date
 
+def change_page(url):
+    url = url.split('&offset=')
+    url[-1] = str(int(url[-1]) + 25)
+    url = '&offset='.join(url)
+    return url
 
 if __name__=='__main__':
     import gevent
@@ -58,22 +63,26 @@ if __name__=='__main__':
     fail = []
     #user_agent = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"}
     main_page = 'https://www.sciencedirect.com/search?qs=ORC&origin=article&zone=qSearch&years=2018%2C2017%2C2016%2C2015&offset=0'
-    main_res = requests.get(main_page)
-    soup = BeautifulSoup(main_res.text, "lxml")
-    paper_area = soup.select('div[class="ResultList col-xs-24"]')[0]
-    paper_blocks = paper_area.select('div[class="result-item-content"]')
-    titles = []
-    for paper_block in paper_blocks:
-        title = paper_block.select('h2 a')[0].text
-        attr = set(paper_block.select('ol[class="SubType hor"]')[0].text.split(' '))
-        date_year = index_date(attr)
-        filename = date_year + title
-        src_url_index = paper_block.select('li[class="DownloadPdf download-link-item"]')[0].a['href']
-        src_url = src_url_root + src_url_index
-        task = [gevent.spawn(download, filename, src_url)]
-#        if err != None:
-#            fail.append(err)
-    result = gevent.joinall(task)
-    print(fail)
+    for i in range(129):
+        print('\n', main_page.split('&offset=')[-1])
+        main_page = change_page(main_page)
+        main_res = requests.get(main_page)
+        soup = BeautifulSoup(main_res.text, "lxml")
+        paper_area = soup.select('div[class="ResultList col-xs-24"]')[0]
+        paper_blocks = paper_area.select('div[class="result-item-content"]')
+        titles = []
+        for paper_block in paper_blocks:
+            title = paper_block.select('h2 a')[0].text
+            attr = set(paper_block.select('ol[class="SubType hor"]')[0].text.split(' '))
+            date_year = index_date(attr)
+            filename = date_year + title
+            src_url_index = paper_block.select('li[class="DownloadPdf download-link-item"]')[0].a['href']
+            src_url = src_url_root + src_url_index
+            task = [gevent.spawn(download, filename, src_url)]
+    #        if err != None:
+    #            fail.append(err)
+        gevent.joinall(task)
+
+    #    print(fail)
 
 
